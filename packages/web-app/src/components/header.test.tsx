2b7deae65d9ebb7c51482b22101c16b95eaafc84
Victor Glindås <glinkis@gmail.com>
2019-03-02 20:43:27 +0100

    Clean up redundant stuff

--- packages/web-app/src/components/header.test.tsx
+++ packages/web-app/src/components/header.test.tsx
@@ -6,7 +6,7 @@ import Header from './header'
 describe('Header', () => {
   it('renders title', () => {
     const text = 'Hello World'
-    const { getByText } = render(<Header>{text}</Header>)
-    expect(getByText(text)).toBeTruthy()
+    const result = render(<Header>{text}</Header>)
+    expect(result.getByText(text)).toBeTruthy()
   })
 })
