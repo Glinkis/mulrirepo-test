206b2f91560b4d9c71d064323ec8612ad512394f
Victor Glindås <glinkis@gmail.com>
2019-03-02 16:00:15 +0100

    Add styling with emotion

--- packages/web-app/src/pages/index.tsx
+++ packages/web-app/src/pages/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { css } from '@emotion/core'
+
+const style = css`
+  margin: 0;
+  padding: 1em;
+  font-family: sans-serif;
+  color: hotpink;
+  background: black;
+`
 
 const IndexPage = () => {
-  return <h1>Hi people</h1>
+  return <h1 css={style}>Hi people</h1>
 }
 
 export default IndexPage
