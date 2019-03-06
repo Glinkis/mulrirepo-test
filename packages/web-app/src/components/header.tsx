235c9fda6de870e1a72605b3f55cf5ac1287ad22
Victor Glindås <glinkis@gmail.com>
2019-03-06 22:23:18 +0100

    Refactor header

--- packages/web-app/src/components/header.tsx
+++ packages/web-app/src/components/header.tsx
@@ -1,29 +1,24 @@
 import React from 'react'
-import { Link } from 'gatsby'
 import styled from '../utils/styled'
-
-const Header = styled.header`
-  background: ${props => props.theme.colors.primary};
-  transition: all 500ms;
-
-  > h1 {
-    padding: 1.5rem;
-
-    > a {
-      color: white;
-      text-decoration: none;
-    }
-  }
-`
 
 interface Props {
   children?: React.ReactNode
 }
 
-export default ({ children }: Props) => (
-  <Header>
-    <h1>
-      <Link to="/">{children}</Link>
-    </h1>
-  </Header>
+const Wrapper = styled.header`
+  background: ${props => props.theme.colors.primary};
+  transition: all 500ms;
+`
+
+const Title = styled.h1`
+  color: white;
+  padding: 1.5rem;
+`
+
+const Header = ({ children }: Props) => (
+  <Wrapper>
+    <Title>{children}</Title>
+  </Wrapper>
 )
+
+export default Header
